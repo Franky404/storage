b82a02620d4c1913708653ca7e8da343fa7ab974
Frangky <81846311+Franky404@users.noreply.github.com>
2021-10-21 10:11:45 +0700

    Create ppcouple.js

---
+++ ppcouple.js
@@ -0,0 +1,24 @@
+var male = document.getElementById("male")
+    var female = document.getElementById("female")
+    var cr = document.querySelector('.carousel-inner')
+    $.getJSON("https://raw.githubusercontent.com/yogipw/yogipw/main/ppcouple.json",
+      function (data) {
+        const random_ppcp = data[Math.floor(Math.random() * data.length)]
+        cr.innerHTML = `
+        <div class="carousel-item">
+        <img src="${random_ppcp.female}" class="d-block w-100" alt="Female">
+        <div class="carousel-caption d-md-block">
+        <h9>Female</h9>
+        <a href="${random_ppcp.female}" target="_blank"download=true>Download</a>
+
+        </div>
+        </div>
+        <div class="carousel-item active">
+        <img src="${random_ppcp.male}" class="d-block w-100" alt="Male">
+        <div class="carousel-caption d-md-block">
+        <h9>Male</h9>
+        <a href="${random_ppcp.male}" target="_blank"download=true>Download</a>
+        </div>
+        </div>
+        `
+      })
